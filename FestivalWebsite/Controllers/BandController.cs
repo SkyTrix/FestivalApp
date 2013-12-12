8e9c34bd4eac11f9b2fcb3b53dff4a694c2a322f
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-12 19:05:39 +0100

    Display bands with image and description

---
+++ FestivalWebsite/Controllers/BandController.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FestivalWebsite.Controllers
+{
+    public class BandController : Controller
+    {
+        //
+        // GET: /Band/
+
+        public ActionResult Index()
+        {
+            IEnumerable<Band> bands = BandManager.Instance.Bands;
+            return View(bands);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Band band = BandManager.Instance.Bands.Where(x => x.ID == id.ToString()).FirstOrDefault();
+            if (band == null)
+                return HttpNotFound();
+
+            return View(band);
+        }
+    }
+}
