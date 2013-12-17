3940973410ffb80c074ebdd2bea2d8d8587358fd
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-17 12:28:45 +0100

    Add generation of tickets

--- FestivalApp/ViewModel/TicketingVM.cs
+++ FestivalApp/ViewModel/TicketingVM.cs
@@ -1,13 +1,18 @@
 ﻿using DAL;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 using FestivalApp.View;
 using GalaSoft.MvvmLight.Command;
 using Models;
+using Ookii.Dialogs.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace FestivalApp.ViewModel
@@ -150,6 +155,91 @@ namespace FestivalApp.ViewModel
             {
                 // Force reload of ticket type UI
                 TicketTypes = null;
+            }
+        }
+
+        public ICommand GenerateTicketsCommand
+        {
+            get { return new RelayCommand(GenerateTickets, CanGenerateTickets); }
+        }
+
+        private bool CanGenerateTickets()
+        {
+            return TicketManager.Tickets != null && TicketManager.Tickets.Count > 0;
+        }
+
+        private void GenerateTickets()
+        {
+            try
+            {
+                VistaFolderBrowserDialog ofd = new VistaFolderBrowserDialog();
+                ofd.Description = "Waar wenst u de tickets op te slaan?";
+                ofd.UseDescriptionForTitle = true;
+                if (ofd.ShowDialog() == true)
+                {
+                    foreach (Ticket ticket in TicketManager.Tickets)
+                    {
+                        string filename = ofd.SelectedPath + Path.DirectorySeparatorChar + ticket.ID + "_" + ticket.TicketHolder + ".docx";
+                        File.Copy("ReservationTemplate.docx", filename, true);
+
+                        WordprocessingDocument newdoc = WordprocessingDocument.Open(filename, true);
+                        IDictionary<String, BookmarkStart> bookmarks = new Dictionary<String, BookmarkStart>();
+
+                        foreach (BookmarkStart bms in newdoc.MainDocumentPart.RootElement.Descendants<BookmarkStart>())
+                        {
+                            bookmarks[bms.Name] = bms;
+                        }
+
+                        Run run = new Run(new Text("TK" + ticket.ID.ToString()));
+                        RunProperties prop = new RunProperties();
+                        RunFonts font = new RunFonts() { Ascii = "Free 3 of 9 Extended", HighAnsi = "Free 3 of 9 Extended" };
+                        FontSize size = new FontSize() { Val = "192" };
+                        prop.Append(font);
+                        prop.Append(size);
+                        run.PrependChild<RunProperties>(prop);
+                        bookmarks["TicketNumber"].Parent.InsertAfter<Run>(run, bookmarks["TicketNumber"]);
+
+                        Run run2 = new Run(new Text(ticket.TicketType.Name));
+                        RunProperties prop2 = new RunProperties();
+                        FontSize size2 = new FontSize() { Val = "72" };
+                        prop2.Append(size2);
+                        run2.PrependChild<RunProperties>(prop2);
+                        bookmarks["TicketType"].Parent.InsertAfter<Run>(run2, bookmarks["TicketType"]);
+
+                        Run run3 = new Run(new Text(ticket.TicketHolder));
+                        RunProperties prop3 = new RunProperties();
+                        FontSize size3 = new FontSize() { Val = "28" };
+                        prop3.Append(size3);
+                        run3.PrependChild<RunProperties>(prop3);
+                        bookmarks["TicketHolder"].Parent.InsertAfter<Run>(run3, bookmarks["TicketHolder"]);
+
+                        Run run4 = new Run(new Text(ticket.TicketHolderEmail));
+                        RunProperties prop4 = new RunProperties();
+                        FontSize size4 = new FontSize() { Val = "28" };
+                        prop4.Append(size4);
+                        run4.PrependChild<RunProperties>(prop4);
+                        bookmarks["TicketHolderEmail"].Parent.InsertAfter<Run>(run4, bookmarks["TicketHolderEmail"]);
+
+                        Run run5 = new Run(new Text("Aantal tickets: " + ticket.Amount));
+                        RunProperties prop5 = new RunProperties();
+                        FontSize size5 = new FontSize() { Val = "28" };
+                        prop5.Append(size5);
+                        run5.PrependChild<RunProperties>(prop5);
+                        bookmarks["Amount"].Parent.InsertAfter<Run>(run5, bookmarks["Amount"]);
+
+                        Run run6 = new Run(new Text(ticket.TicketType.Price.ToString("C2")));
+                        RunProperties prop6 = new RunProperties();
+                        FontSize size6 = new FontSize() { Val = "28" };
+                        prop6.Append(size6);
+                        run6.PrependChild<RunProperties>(prop6);
+                        bookmarks["Price"].Parent.InsertAfter<Run>(run6, bookmarks["Price"]);
+
+                        newdoc.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
             }
         }
     }
