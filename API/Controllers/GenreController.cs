3d6f9079166703b101af65220bcca3ead4575f52
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-25 15:52:38 +0100

    Add api controllers for lineup, bands and genres

---
+++ API/Controllers/GenreController.cs
@@ -0,0 +1,41 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class GenreController : ApiController
+    {
+        // GET api/genre
+        public IEnumerable<Genre> Get()
+        {
+            return GenreManager.Instance.Genres;
+        }
+
+        // GET api/genre/5
+        public Genre Get(int id)
+        {
+            return GenreManager.Instance.Genres.ToList().Find(x => x.ID == id);
+        }
+
+        // POST api/genre
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/genre/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/genre/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Controllers/LineUpController.cs
@@ -0,0 +1,41 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class LineUpController : ApiController
+    {
+        // GET api/lineup
+        public IEnumerable<LineUpItem> Get()
+        {
+            return LineUpManager.Instance.LineUpItems;
+        }
+
+        // GET api/lineup/5
+        public LineUpItem Get(int id)
+        {
+            return LineUpManager.Instance.LineUpItems.ToList().Find(x => x.ID == id);
+        }
+
+        // POST api/lineup
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/lineup/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/lineup/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
