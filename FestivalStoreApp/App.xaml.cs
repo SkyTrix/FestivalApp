79e3fcf50b5aca7d1060ebbac086cd1a2079d2cf
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-26 12:26:56 +0100

    Add search with search suggestions for bands and genres to the store app

--- FestivalStoreApp/App.xaml.cs
+++ FestivalStoreApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FestivalStoreApp.Common;
+﻿using Windows.ApplicationModel.Search;
+using FestivalStoreApp.Common;
 using FestivalStoreApp.DataModel;
 using Models;
 using System;
@@ -29,6 +30,8 @@ namespace FestivalStoreApp
     /// </summary>
     sealed partial class App : Application
     {
+        private List<string> _searchTerms;
+
         /// <summary>
         /// Initializes the singleton Application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -63,7 +66,7 @@ namespace FestivalStoreApp
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
-            
+
             if (rootFrame == null)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
@@ -88,6 +91,9 @@ namespace FestivalStoreApp
                 // Load recipe data
                 await LineUpDataSource.LoadRemoteDataAsync();
 
+                // Register handler for SuggestionsRequested events from the search pane
+                SearchPane.GetForCurrentView().SuggestionsRequested += OnSuggestionsRequested;
+
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
@@ -105,6 +111,44 @@ namespace FestivalStoreApp
             Window.Current.Activate();
         }
 
+        void OnSuggestionsRequested(SearchPane sender, SearchPaneSuggestionsRequestedEventArgs args)
+        {
+            string query = args.QueryText.ToLower();
+
+            if (_searchTerms == null)
+            {
+                _searchTerms = new List<string>();
+
+                IEnumerable<LineUpDataGroup> groups = LineUpDataSource.GetGroups("AllGroups");
+
+                // Add all band names and genres to the search terms
+                foreach (LineUpDataGroup group in groups)
+                {
+                    foreach (LineUpItem item in group.Items)
+                    {
+                        if (!_searchTerms.Contains(item.Band.Name))
+                        {
+                            _searchTerms.Add(item.Band.Name);
+                        }
+
+                        foreach (Genre genre in item.Band.Genres)
+                        {
+                            if (!_searchTerms.Contains(genre.Name))
+                            {
+                                _searchTerms.Add(genre.Name);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var term in _searchTerms)
+            {
+                if (term.ToLower().Contains(query))
+                    args.Request.SearchSuggestionCollection.AppendQuerySuggestion(term);
+            }
+        }
+
         /// <summary>
         /// Invoked when application execution is being suspended.  Application state is saved
         /// without knowing whether the application will be terminated or resumed with the contents
@@ -117,6 +161,63 @@ namespace FestivalStoreApp
             var deferral = e.SuspendingOperation.GetDeferral();
             await SuspensionManager.SaveAsync();
             deferral.Complete();
+        }
+
+        /// <summary>
+        /// Invoked when the application is activated to display search results.
+        /// </summary>
+        /// <param name="args">Details about the activation request.</param>
+        protected async override void OnSearchActivated(Windows.ApplicationModel.Activation.SearchActivatedEventArgs args)
+        {
+            // Reinitialize the app if a new instance was launched for search
+            if (args.PreviousExecutionState == ApplicationExecutionState.NotRunning ||
+                args.PreviousExecutionState == ApplicationExecutionState.ClosedByUser ||
+                args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                // Load recipe data
+                await LineUpDataSource.LoadRemoteDataAsync();
+
+                // Register handler for SuggestionsRequested events from the search pane
+                SearchPane.GetForCurrentView().SuggestionsRequested += OnSuggestionsRequested;
+            }
+
+            // TODO: Register the Windows.ApplicationModel.Search.SearchPane.GetForCurrentView().QuerySubmitted
+            // event in OnWindowCreated to speed up searches once the application is already running
+
+            // If the Window isn't already using Frame navigation, insert our own Frame
+            var previousContent = Window.Current.Content;
+            var frame = previousContent as Frame;
+
+            // If the app does not contain a top-level frame, it is possible that this 
+            // is the initial launch of the app. Typically this method and OnLaunched 
+            // in App.xaml.cs can call a common method.
+            if (frame == null)
+            {
+                // Create a Frame to act as the navigation context and associate it with
+                // a SuspensionManager key
+                frame = new Frame();
+                FestivalStoreApp.Common.SuspensionManager.RegisterFrame(frame, "AppFrame");
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // Restore the saved session state only when appropriate
+                    try
+                    {
+                        await FestivalStoreApp.Common.SuspensionManager.RestoreAsync();
+                    }
+                    catch (FestivalStoreApp.Common.SuspensionManagerException)
+                    {
+                        //Something went wrong restoring state.
+                        //Assume there is no state and continue
+                    }
+                }
+            }
+
+            frame.Navigate(typeof(SearchResultsPage), args.QueryText);
+            Window.Current.Content = frame;
+
+            // Ensure the current window is active
+            Window.Current.Activate();
         }
     }
 }
