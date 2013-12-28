d34e13c59539e8d7c4d4d1ad5080bcd7e6ff3b30
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-28 20:12:09 +0100

    Display ticket reservations for admin

---
+++ FestivalWebsite/Controllers/TicketController.cs
@@ -0,0 +1,28 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FestivalWebsite.Controllers
+{
+    public class TicketController : Controller
+    {
+        //
+        // GET: /Ticket/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Reservations()
+        {
+            var tickets = TicketManager.GetTickets();
+            return View(tickets);
+        }
+    }
+}
