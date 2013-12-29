74ba74ddd8fbcc0eee1072c5d60d99ced7b57e05
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2013-12-29 19:44:22 +0100

    Add ticket types and revenue info to admin page

--- FestivalWebsite/Controllers/TicketController.cs
+++ FestivalWebsite/Controllers/TicketController.cs
@@ -1,4 +1,5 @@
 ﻿using DAL;
+using FestivalWebsite.ViewModels;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -21,8 +22,33 @@ namespace FestivalWebsite.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult Reservations()
         {
+            List<TicketTypeVM> ticketTypes = new List<TicketTypeVM>();
+            foreach (TicketType ticketType in TicketTypeManager.GetTicketTypes())
+            {
+                ticketTypes.Add(new TicketTypeVM()
+                {
+                    TicketType = ticketType,
+                    RemainingTickets = TicketTypeManager.CountTicketsRemainingForTicketType(ticketType.ID)
+                });
+            }
+
             var tickets = TicketManager.GetTickets();
-            return View(tickets);
+
+            double totalRevenue = 0;
+
+            foreach(Ticket ticket in tickets)
+            {
+                totalRevenue += ticket.TicketType.Price * double.Parse(ticket.Amount);
+            }
+
+            ReservationsOverviewVM model = new ReservationsOverviewVM()
+            {
+                Tickets = tickets,
+                TicketTypes = ticketTypes,
+                TotalRevenue = totalRevenue
+            };
+
+            return View(model);
         }
     }
 }
---
+++ FestivalWebsite/ViewModels/ReservationsOverviewVM.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FestivalWebsite.ViewModels
+{
+    public class ReservationsOverviewVM
+    {
+        public IEnumerable<Ticket> Tickets { get; set; }
+        public IEnumerable<TicketTypeVM> TicketTypes { get; set; }
+        public double TotalRevenue { get; set; }
+    }
+}
